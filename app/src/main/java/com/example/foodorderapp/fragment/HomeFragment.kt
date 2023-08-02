5304beafcafd8b80cfbe26318afdbea5cc0c4efe
Monu <105378383+PROGRAMMINGKING8055@users.noreply.github.com>
2023-08-02 12:44:02 +0530

    FoodOrder Android App Using Kotlin

--- app/src/main/java/com/example/foodorderapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/foodorderapp/fragment/HomeFragment.kt
@@ -5,22 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.foodorderapp.R
+import com.example.foodorderapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
-
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
+
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+        imageSlider.setItemClickListener(object : ItemClickListener {
+            override fun doubleClick(position: Int) {
+
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+                val itemMassage = "Selected image $position"
+                Toast.makeText(requireContext(), itemMassage,Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     companion object {
